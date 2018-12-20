de7f5ea994b6dc95e0a6da8523f5add6a4c4c8a4
Fred Caton <fcaton@anthemengineering.com>
2018-12-20 13:36:28 -0800

    Disable data download with every build
    In order for libpostal to work, it needs to access training data.
    Download the training data to an external file system using the
    libpostal_data shell script in the src directory of the libpostal repo.
    Mount the data to /libpostal/libpostal when running the container.

--- install-libpostal.sh
+++ install-libpostal.sh
@@ -5,7 +5,6 @@ git clone https://github.com/openvenues/libpostal
 git checkout tags/v1.0.0
 cd /libpostal
 ./bootstrap.sh
-./configure --datadir=$PWD 
+./configure --datadir=$PWD --disable-data-download
 make -j4
 make install
-make clean
